a95c4caf4489fa24266bf78e85edb1a71307049a
Younes Karim <younes-e@hotmail.com>
2021-11-10 10:32:40 +0100

    local changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import facade from "./apiFacade";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import Header from "./components/Header";
-import CatFacts from "./components/CatFacts";
-import Home from "./components/Home"
-import Products from "./components/Products";
-
-// import './style2.css';
+import Nesting from "./components/NestedRoutes";
 
 function LogIn({ login }) {
   const init = { username: "", password: "" };
@@ -19,127 +9,62 @@ function LogIn({ login }) {
   const performLogin = (evt) => {
     evt.preventDefault();
     login(loginCredentials.username, loginCredentials.password);
-  }
+  };
   const onChange = (evt) => {
-    setLoginCredentials({ ...loginCredentials, [evt.target.id]: evt.target.value })
-  }
+    setLoginCredentials({
+      ...loginCredentials,
+      [evt.target.id]: evt.target.value,
+    });
+  };
 
   return (
     <div>
       <h2>Login</h2>
-      <form onChange={onChange} >
+      <form onChange={onChange}>
         <input placeholder="User Name" id="username" />
         <input placeholder="Password" id="password" />
         <button onClick={performLogin}>Login</button>
       </form>
     </div>
-  )
-
+  );
 }
 function LoggedIn() {
-  const [dataFromServer, setDataFromServer] = useState("Loading...")
+  const [dataFromServer, setDataFromServer] = useState("Loading...");
 
-  useEffect(() => { facade.fetchData().then(data=> setDataFromServer(data.msg));
-  }, [])
+  useEffect(() => {
+    facade.fetchData().then((data) => setDataFromServer(data.msg));
+  }, []);
 
-
-  return (   
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-          <Route path="/catfact">
-            <CatFacts />
-          </Route>
-          <Route path="/add-Book">
-            <AddBook />
-          </Route>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
-      
-      <h2>Data Received from server</h2>
-      <h3>{dataFromServer}</h3>
+  return (
+    <div>
+      <h4>{dataFromServer}</h4>
+      <Nesting />
     </div>
-    
   );
-
 }
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const logout = () => {
-    facade.logout()
-    setLoggedIn(false)
-  }
+    facade.logout();
+    setLoggedIn(false);
+  };
   const login = (user, pass) => {
-    facade.login(user, pass)
-      .then(res => setLoggedIn(true));
-  }
+    facade.login(user, pass).then((res) => setLoggedIn(true));
+  };
 
   return (
     <div>
-      {!loggedIn ? (<LogIn login={login} />) :
-        (<div>
+      {!loggedIn ? (
+        <LogIn login={login} />
+      ) : (
+        <div>
           <LoggedIn />
           <button onClick={logout}>Logout</button>
-        </div>)}
+        </div>
+      )}
     </div>
-  )
-
+  );
 }
 export default App;
-
-//Alt herunder er kun til testing. Meningen er det også skal flyttes ind i et component for sig.
-
-
-
-
-
-
-const AddBook = (props) => {
-  const [book, setBook] = useState({ title: "", info: "" });
-
-  const changeHandler = (evt) => {
-    setBook({ ...book, [evt.target.id]: evt.target.value });
-  };
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    props.bookFacade.addBook(book);
-  };
-
-  return (
-    <>
-      <h2>Add Book</h2>
-      <form onChange={changeHandler} onSubmit={handleSubmit}>
-        <label htmlFor="title">add title:</label>
-        <br />
-        <input type="text" id="title" name="title" placeholder="Add title" />
-        <br />
-        <label htmlFor="info">Info:</label>
-        <br />
-        <input type="text" id="info" name="info" placeholder="Add info" />
-        <button type="submit">Save</button>
-      </form>
-      <p>{JSON.stringify(book)}</p>
-    </>
-  );
-};
-
-
-
-const NoMatch = () => {
-  return (
-    <div>
-      <h3>404</h3>
-    </div>
-  );
-};
---
+++ src/components/NestedRoutes.jsx
@@ -0,0 +1,120 @@
+import "../css/style2.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import CatFacts from "./CatFacts";
+import HomeNested from "./HomeNested";
+// import Products from "./Products";
+
+export default function Nesting() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/topics">
+              <Topics />
+            </Route>
+            <Route path="/catfacts">
+              <CatFacts />
+            </Route>
+            <Route path="/products">{/* <Products /> */}</Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+const Header = () => {
+  return (
+    <ul className="header">
+      <li>
+        <NavLink exact activeClassName="selected" to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="selected" to="/topics">
+          Topics
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="selected" to="/catfacts">
+          Cat Facts
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="selected" to="/products">
+          Products
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+function Home() {
+  return (
+    <div>
+      <HomeNested />
+    </div>
+  );
+}
+
+function Topics() {
+  // The `path` lets us build <Route> paths that are
+  // relative to the parent route, while the `url` lets
+  // us build relative links.
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  // The <Route> that rendered this component has a
+  // path of `/topics/:topicId`. The `:topicId` portion
+  // of the URL indicates a placeholder that we can
+  // get from `useParams()`.
+  let { topicId } = useParams();
+
+  return (
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}
