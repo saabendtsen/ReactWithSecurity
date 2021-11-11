0ced024d9331f84fa419409088f14c89245ae478
Søren Aagaard Bendtsen <soren@tech-logic.dk>
2021-11-11 11:16:19 +0100

    bitcoin page

--- src/components/GetBTCPrice.jsx
+++ src/components/GetBTCPrice.jsx
@@ -1,37 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { bitCoinPrice_URL } from "./Urls";
-class GetBTCPrice extends React.Component {
-  // Constructor
-  constructor(props) {
-    super(props);
+const GetBTCPrice = () => {
+  const [items, setItems] = useState();
+  const [DataisLoaded, setDataisLoaded] = useState(false)
+  const [coinList, setCoinList] = useState([])
+  const [selectedCoin, setSelectedCoin] = useState();
+  let id;
 
-    this.state = {
-      items: [],
-      DataisLoaded: false,
-    };
-  }
-
-  // ComponentDidMount is used to
-  componentDidMount() {
+  useEffect(() => {
+    setDataisLoaded(false)
     fetch(bitCoinPrice_URL)
       .then((res) => res.json())
-
       .then((json) => {
-        this.setState({ items: json, DataisLoaded: true });
-      });
+        setItems(json);
+        setDataisLoaded(true)
+      })
+      setCoinList([])
+    fetch("https://api.coingecko.com/api/v3/search/trending")
+      .then((res) => res.json())
+      .then((json) => {
+        json.coins.map(el => {
+          const coin = {symbol: el.item.symbol, id:el.item.name};
+          coinList.push(coin)
+        })
+        setCoinList(coinList);
+    })}, []);
+
+  const seachCoin = (evt) =>{
+      evt.preventDefault();
+      fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=usd&include_market_cap=true&include_24hr_change=true`)
+      .then((res) => res.json())
+      .then((json) =>{
+        setSelectedCoin(json);
+        setDataisLoaded(true);
+      })
   }
 
-  render() {
-    const { DataisLoaded, items } = this.state;
-    if (!DataisLoaded)
-      return (
-        <div>
-          <h1> Fetching Data! Please wait.... </h1>{" "}
-        </div>
-      );
 
-    return <div>{JSON.stringify(items)}</div>;
-  }
+  if (!DataisLoaded)
+    return (
+      <div>
+        <h1> Fetching Data! Please wait.... </h1>{" "}
+      </div>
+    );
+
+  return <div>{JSON.stringify(selectedCoin)}
+    <h4> Current price on BTC in USD: {items.bpi.USD.rate}</h4>
+    <div>
+      <datalist id="suggestions">
+        {coinList.map((el,idx) =>{
+          return(
+        <option key={idx}>{el.id}</option>
+        )})}
+      </datalist>
+      <input onChange={(evt)=> id = evt.target.value} autoComplete="on" list="suggestions" /> <button onClick={seachCoin} type="submit">Seach coin Price</button>
+      
+    </div>
+
+  </div>;
 }
 
 export default GetBTCPrice;
